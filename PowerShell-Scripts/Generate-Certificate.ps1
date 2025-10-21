ec55c4b41233b0be33853f9f82ae404284cac18d
Srujan Vemuri <vemurisaisrujan@gmail.com>
2025-10-21 19:07:14 +0530

    Create Generate-Certificate.ps1

---
+++ PowerShell-Scripts/Generate-Certificate.ps1
@@ -0,0 +1,11 @@
+﻿# Generate a self-signed certificate for SAML
+$cert = New-SelfSignedCertificate -Subject "CN=<domain url>" -KeyAlgorithm RSA -KeyLength 3072 `
+  -KeyUsage DigitalSignature, KeyEncipherment -NotAfter (Get-Date).AddYears(3) `
+  -CertStoreLocation Cert:\CurrentUser\My -KeyExportPolicy Exportable
+
+# Export cert to PFX (with password)
+$mypwd = ConvertTo-SecureString "<password>" -AsPlainText -Force
+Export-PfxCertificate -Cert $cert -FilePath "$env:USERPROFILE\Desktop\saml-signing.pfx" -Password $mypwd
+
+# Export public certificate (CER)
+Export-Certificate -Cert $cert -FilePath "$env:USERPROFILE\Desktop\saml-signing.cer"
